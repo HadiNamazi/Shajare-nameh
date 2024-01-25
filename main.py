066070dd4006cfcf9747ef32c8fb99c42396a9ca
Attachments90 <123238819+Attachments90@users.noreply.github.com>
2024-01-25 21:52:05 +0330

    Updated main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
 def sha256(input_str):
     K = [
         0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
